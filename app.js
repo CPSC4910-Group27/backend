e597393642f162a8b115813ecaf2e823ec2600df
Gabe Volheim <gabevolheim@gmail.com>
2024-03-08 18:46:02 -0500

    Fixed Driver Endpoint add response code 200

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get('/sponsorusers/all', async (req, res) => {
   app.get('/drivers', async (req, res) => {
     try {
       const { rows } = await connection.query('SELECT * FROM Drivers');
-      res.json(rows);
+      res.status(200).json(rows);
       return;
     } catch (error) {
       console.error('Error fetching drivers:', error);
