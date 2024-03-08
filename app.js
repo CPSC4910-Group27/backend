8233829bad969e4ae1bf5e0a119f8e13f9fc4cfd
Gabe Volheim <gabevolheim@gmail.com>
2024-03-08 18:57:53 -0500

    Fixed Driver Endpoint add response code 200

--- app.js
+++ app.js
@@ -95,16 +95,19 @@ app.get('/sponsorusers/all', async (req, res) => {
   
   // get all drivers
   app.get('/drivers', async (req, res) => {
-    try {
+    
         query = 'SELECT * FROM Drivers';
-        await connection.query(query,rows);
-        res.status(200).json(rows);
-        return;
-    } catch (error) {
-      console.error('Error fetching drivers:', error);
-      res.status(500).json({ error: 'Internal server error' });
-      return;
-    }
+        connection.query(query,(queryError, result)=> {
+            if(queryError){
+                console.error('Error fetching drivers:', error);
+                res.status(500).json({ error: 'Internal server error' });
+                return;
+            }
+            else{
+                res.status(200).json(result);
+                return;
+            }
+        });
   });
   
   // get all applications associated with a sponsor
