3f62c0632ee6cdc501f2ecafabe974bbe6a3471c
GabeVolheim <gabevolheim@gmail.com>
2024-03-28 11:52:16 -0400

    Fixed get /application sql error

--- app.js
+++ app.js
@@ -346,7 +346,7 @@ app.get('/applications', async (req, res) => {
      // RETURNS ALL APPLICATIONS FOR SPECIFIC USER WITH A SPECIFIC STATUS
      else if (USER_ID && STATUS)
      {
-         query = 'SELECT * FROM Application A JOIN Users U on A.USER_ID = U.USER_ID WHERE USER_ID = ' + USER_ID.toString() + ' AND STATUS LIKE ?';
+         query = 'SELECT * FROM Application A JOIN Users U on A.USER_ID = U.USER_ID WHERE A.USER_ID = ' + USER_ID.toString() + ' AND STATUS LIKE ?';
          connection.query(query,[STATUS],(queryError, result)=> {
              if(queryError){
                  console.error('Error fetching application associated with ${sponsorID}:', queryError);
@@ -363,7 +363,7 @@ app.get('/applications', async (req, res) => {
     // RETURNS ALL APPLICATIONS FOR SPECIFIC USER
     else if (USER_ID)
     {
-        query = 'SELECT * FROM Application A JOIN Users U on A.USER_ID = U.USER_ID WHERE USER_ID = ' + USER_ID.toString();
+        query = 'SELECT * FROM Application A JOIN Users U on A.USER_ID = U.USER_ID WHERE A.USER_ID = ' + USER_ID.toString();
         connection.query(query,(queryError, result)=> {
             if(queryError){
                 console.error('Error fetching application associated with ${sponsorID}:', queryError);
