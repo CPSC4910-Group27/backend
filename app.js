78b63fa79eb4e5d832ac84a0dd9c677401ecba88
GabeVolheim <gabevolheim@gmail.com>
2024-04-09 17:32:39 -0400

    ebay work

--- app.js
+++ app.js
@@ -94,6 +94,7 @@ app.get('/ebay', (req, res) =>{
                 headers: headers
             }).then(response => {
                 if (!response.ok) {
+                    console.log(response);
                     res.status(500).json({error: 'Internal server error'});
                 }
                 return response.json();
